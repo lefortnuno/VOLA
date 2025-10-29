fb2beb5b2a56ddb853674ac8f9245e75a2e1b6e5
lefortnuno <trofelnuno@gmail.com>
2025-10-29 17:51:33 +0100

    fix: Contrainte de validation dans MENU

--- vola.c
+++ vola.c
@@ -7,19 +7,34 @@ int choix = 0;
 
 int main() {  
     time_t maintenant = time(NULL);
+    
+    int saisie_valide;
     do {
-        printf("\n  ================ GESTION DEPENSES ===============\n");
-        printf("|| 1.Ajouter ");
-        printf("|| 2.Historiq ");
-        printf("|| 3.Total "); 
-        printf("|| 0.Quitter ||");
-        printf("\n  =================================================\n");
-        printf("||%-6s 4.UpRevenus %-6s", spc, spc);
-        printf("||%-4s 5.UpCharges %-5s||", spc, spc);
-        printf("\n  =================================================\n");
-        printf("|| Choix: "); 
-        fflush(stdout); 
-        scanf("%d", &choix);
+        do {
+            printf("\n  ================ GESTION DEPENSES ===============\n");
+            printf("|| 1.Ajouter ");
+            printf("|| 2.Historiq ");
+            printf("|| 3.Total "); 
+            printf("|| 0.Quitter ||");
+            printf("\n  =================================================\n");
+            printf("||%-6s 4.UpRevenus %-6s", spc, spc);
+            printf("||%-4s 5.UpCharges %-5s||", spc, spc);
+            printf("\n  =================================================\n");
+            printf("|| Choix: "); 
+            fflush(stdout); 
+
+            if (scanf("%d", &choix) != 1) { 
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
+                saisie_valide = 0;
+            } else {
+                saisie_valide = (choix >= 0 && choix <= 5);
+            }
+            
+            if (!saisie_valide) {
+                choix_invalide();
+            }
+        } while (!saisie_valide); 
 
         switch (choix) {
             case 1: add_depense(); break;
